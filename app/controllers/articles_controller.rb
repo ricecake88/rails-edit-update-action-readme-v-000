907a972bcc91816733ae7ac6f13ddc1d3cfbc2b5
Grace Shih <vocalise@gmail.com>
2019-01-09 04:33:55 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,6 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
-    binding.pry
   end
 
   def create
@@ -27,8 +26,7 @@ class ArticlesController < ApplicationController
   
   def update
     @article = Article.find(params[:id])
-    binding.pry
     @article.update(:title => params[:title], :description => params[:description])
-    redirect_to article_path
+    redirect_to article_path(@article)
   end
 end
